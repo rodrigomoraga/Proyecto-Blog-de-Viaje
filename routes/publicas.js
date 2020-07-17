883801bc811afee846bcbbc07c6dab758182560f
Rodrigo <rodrigomoraga45@gmail.com>
2020-07-17 16:23:39 -0400

    se crea sistema privado

---
+++ routes/publicas.js
@@ -0,0 +1,111 @@
+const express = require('express')
+const router = express.Router()
+const mysql = require('mysql')
+
+var pool = mysql.createPool({
+  connectionLimit: 20,
+  host: 'localhost',
+  user: 'rodrigo',
+  password: '1qazxsw2',
+  database: 'blog_viajes'
+})
+
+router.get('/', (peticion, respuesta) => {
+  pool.getConnection((err, connection) => {
+    const consulta = `
+      SELECT
+      titulo, resumen, fecha_hora, pseudonimo, votos
+      FROM publicaciones
+      INNER JOIN autores
+      ON publicaciones.autor_id = autores.id
+      ORDER BY fecha_hora DESC
+      LIMIT 5
+    `
+    connection.query(consulta, (error, filas, campos) => {
+      respuesta.render('index', { publicaciones: filas })
+    })
+    connection.release()
+  })
+})
+
+router.get('/registro', (peticion, respuesta) => {
+  respuesta.render('registro', { mensaje: peticion.flash('mensaje') })
+})
+
+router.post('/procesar_registro', (peticion, respuesta) => {
+  pool.getConnection((err, connection) => {
+    const email = peticion.body.email.toLowerCase().trim()
+    const pseudonimo = peticion.body.pseudonimo.trim()
+    const contrasena = peticion.body.contrasena
+    const consultaEmail = `
+      SELECT *
+      FROM autores
+      WHERE email = ${connection.escape(email)}
+    `
+    connection.query(consultaEmail, (error, filas, campos) => {
+      if (filas.length > 0) {
+        peticion.flash('mensaje', 'Email duplicado')
+        respuesta.redirect('/registro')
+      }
+      else {
+        const consultaPseudonimo = `
+          SELECT *
+          FROM autores
+          WHERE pseudonimo = ${connection.escape(pseudonimo)}
+        `
+        connection.query(consultaPseudonimo, (error, filas, campos) => {
+          if (filas.length > 0) {
+            peticion.flash('mensaje', 'Pseudonimo duplicado')
+            respuesta.redirect('/registro')
+          }
+          else {
+            const consulta = `
+                                INSERT INTO
+                                autores
+                                (email, contrasena, pseudonimo)
+                                VALUES (
+                                  ${connection.escape(email)},
+                                  ${connection.escape(contrasena)},
+                                  ${connection.escape(pseudonimo)}
+                                )
+                              `
+            connection.query(consulta, (error, filas, campos) => {
+              peticion.flash('mensaje', 'Usuario registrado')
+              respuesta.redirect('/registro')
+            })
+          }
+        })
+      }
+    })
+    connection.release()
+  })
+})
+
+router.get('/inicio', (peticion, respuesta) => {
+  respuesta.render('inicio', { mensaje: peticion.flash('mensaje') })
+})
+
+router.post('/procesar_inicio', (peticion, respuesta) => {
+  pool.getConnection((err, connection) => {
+    const consulta = `
+      SELECT *
+      FROM autores
+      WHERE
+      email = ${connection.escape(peticion.body.email)} AND
+      contrasena = ${connection.escape(peticion.body.contrasena)}
+    `
+    connection.query(consulta, (error, filas, campos) => {
+      if (filas.length > 0) {
+        peticion.session.usuario = filas[0]
+        respuesta.redirect('/admin/index')
+      }
+      else {
+        peticion.flash('mensaje', 'Datos inválidos')
+        respuesta.redirect('/inicio')
+      }
+    })
+    connection.release()
+  })
+})
+
+module.exports = router
